102a7dd45d76234e9b1773c08eecce2a1c60a21d
Aymen Idouahman <aymanidou@gmail.com>
2024-04-02 01:33:18 +0000

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -33,7 +33,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        ''''''
+        '''get a page of data from the csv file'''
         assert type(page) is int and type(page_size) is int
         assert page > 0 and page_size > 0
         first_idx, last_idx = index_range(page, page_size)
