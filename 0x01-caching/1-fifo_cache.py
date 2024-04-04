b21b8e83b09d6b49f60a3df1bc8e97ec178a5753
Aymen Idouahman <aymanidou@gmail.com>
2024-04-04 00:43:09 +0000

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,7 +18,6 @@ class FIFOCache(BaseCaching):
         """
         if (key is None or item is None):
             return
-
         self.cache_data[key] = item
         if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
             first_key, _ = self.cache_data.popitem(False)
