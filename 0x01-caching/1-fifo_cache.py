d08a85cfb42982780ddbf4848f7c40b3afb30a98
Aymen Idouahman <aymanidou@gmail.com>
2024-04-04 00:19:00 +0000

    1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+'''0x01-caching'''
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    '''FIFOCache'''
+
+    def __init__(self):
+        """Initialize
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if (key is None or item is None):
+            return
+
+        self.cache_data[key] = item
+        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD: {}".format(first_key))
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if (key is None or key not in self.cache_data.keys()):
+            return None
+        return self.cache_data[key]
