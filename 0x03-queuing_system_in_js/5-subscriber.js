4e6cf11cc7c6cc8ed3898d8204c2301c56839c78
Aymen Idouahman <aymanidou@gmail.com>
2024-05-08 23:53:38 +0000

    5. Node Redis client publisher and subscriber

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(`Received message on channel ${channel}: ${message}`);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe('holberton school channel');
+    client.quit();
+  }
+});
